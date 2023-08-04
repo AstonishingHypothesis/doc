0a516cb1fd8ee374b3f594f4f68c9db89fa03054
AstonishingHypothesis <88336439+AstonishingHypothesis@users.noreply.github.com>
2023-08-04 15:07:04 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NeuroPhi'
+copyright = '2023, Maier'
+author = 'Maier'
 
 release = '0.1'
 version = '0.1.0'
